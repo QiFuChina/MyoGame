b87dcf39cfe57876eb43db1f1c2125db19d8b865
QiFuChina <517469138@qq.com>
2018-04-09 22:22:18 +0100

    Delete unnecessary files

--- Game/Assets/Myo/Scripts/Move.cs
+++ Game/Assets/Myo/Scripts/Move.cs
@@ -11,7 +11,13 @@ public class Move : MonoBehaviour
     // Keep track of our jumping state
     // public bool isJumping = false;
 
+
+    // public Collider AtkCollider;
+    // public Animator anim;
+
+    
 	public bool isWalking = false;
+    public bool isattacking = false;
     // Executes on first launch
     void Start()
     {
@@ -23,16 +29,13 @@ public class Move : MonoBehaviour
         while (Application.isPlaying)
         {
             // Adds force when the user makes a fist or presses the enter key
-            if (!isWalking && (Myo.pose == Thalmic.Myo.Pose.Fist || Input.GetKey(KeyCode.Return)))
-            {
-                // var dir = Capsule.transform.position - Capsule.transform.GetChild(0).position * 100;
-                // Capsule.AddForce(dir, ForceMode.Impulse);
+            if (!isWalking && (Myo.pose == Thalmic.Myo.Pose.Fist || Input.GetKey(KeyCode.W)))
+            {        
                 Player.transform.Translate(Vector3.forward*1f);
-                //gameObject.GetComponent<Transform> ().Translate (Vector3.forward * 0.05f, Space.Self);
                 print("Forward");
                 isWalking = true;
             }
-            else if (isWalking && (Myo.pose != Thalmic.Myo.Pose.Fist && !Input.GetKey(KeyCode.Return)))
+            else if (isWalking && (Myo.pose != Thalmic.Myo.Pose.Fist && !Input.GetKey(KeyCode.W)))
             {
                 isWalking = false;
             }
@@ -41,7 +44,6 @@ public class Move : MonoBehaviour
             {
                 Player.transform.Rotate(0f, -2f, 0f);
 				print("Left");
-                //isWalking = true;
             }
             else if (isWalking && (Myo.pose != Thalmic.Myo.Pose.WaveIn && !Input.GetKey(KeyCode.A)))
             {
@@ -51,7 +53,6 @@ public class Move : MonoBehaviour
             {
                 Player.transform.Rotate(0f, 2f, 0f);
 				print("Right");
-                //isWalking = true;
             }
             else if (isWalking && (Myo.pose != Thalmic.Myo.Pose.WaveOut && !Input.GetKey(KeyCode.A)))
             {
@@ -59,10 +60,7 @@ public class Move : MonoBehaviour
             }
 			if (!isWalking && (Myo.pose == Thalmic.Myo.Pose.FingersSpread || Input.GetKey(KeyCode.A)))
             {
-                // var dir = Capsule.transform.position - Capsule.transform.GetChild(0).position * 100;
-                // Capsule.AddForce(dir, ForceMode.Impulse);
                 Player.transform.Translate(Vector3.back*1f);
-				// gameObject.GetComponent<Transform> ().Translate (Vector3.back * 0.05f, Space.Self);
                 print("Back");
                 isWalking = true;
             }
@@ -70,6 +68,17 @@ public class Move : MonoBehaviour
             {
                 isWalking = false;
             }
+            // if (!isattacking && (Myo.pose == Thalmic.Myo.Pose.DoubleTap || Input.GetKey(KeyCode.J)))
+            // {
+            //     anim.SetBool ("Attack", true);
+            //     AtkCollider.GetComponent<SphereCollider>().enabled = true;
+            //     print("Attack");
+            //     isattacking = true;
+            // }
+            // else if (isattacking && (Myo.pose != Thalmic.Myo.Pose.FingersSpread && !Input.GetKey(KeyCode.J)))
+            // {
+            //     isattacking = false;
+            // }
             yield return new WaitForSeconds(0.1f);
         }
     }
